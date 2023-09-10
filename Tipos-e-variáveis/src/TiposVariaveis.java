c93cf885c45b38d6fe75b3ed21cb70a85a2a0cae
Carlos Junior <carlossilvajr.ti@hotmail.com>
2023-09-10 16:41:24 -0300

    TiposVariaveis

--- Tipos-e-variáveis/src/TiposVariaveis.java
+++ Tipos-e-variáveis/src/TiposVariaveis.java
@@ -1,12 +1,10 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-     System.out.println("Treinando Tipos e variáveis");
-        
+     
         String meuNome = "Carlos Junior";
-        double salarioMinimo = 2500;
-
+        int salarioMinimo = 2500;
         int minhaIdade = 37; 
 
-
+         System.out.println("Olá, me chamo " + meuNome + ". Tenho um salario de " + salarioMinimo + " e minha idade é " + minhaIdade);
     }
 }
