ef7dd1ab37a2281f8996f2ba36f05493cf129a69
Carlos Junior <carlossilvajr.ti@hotmail.com>
2023-09-21 23:40:18 -0300

    Meu Projeto Conta Banco

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    int Numero;
+    String Agencia;
+    String NomeCliente;
+    Float Saldo;
+
+
+
+    public static void main(String[] args) throws Exception{
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        Int Numero = scanner.next();
+    
+
+
+
+    }
+    
+}
