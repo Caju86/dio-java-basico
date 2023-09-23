c1759a23c697a4454aecaa2fd0f44de2b86dea74
Carlos Junior <carlossilvajr.ti@hotmail.com>
2023-09-23 16:35:00 -0300

    Conta-Terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -6,8 +6,6 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception{
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Bom dia!!!");
-
         System.out.println("Por favor, digite o número da Agência: ");
          String Agencia = scanner.next();
         
